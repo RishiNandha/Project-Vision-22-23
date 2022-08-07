918a409e3425c4088451dc26a12bf211eab27127
Rishi Nandha Vanchi <49914358+RishiNandha@users.noreply.github.com>
2022-08-07 10:48:56 +0530

    fff

--- Prelims/tri_hog.py
+++ Prelims/tri_hog.py
@@ -9,10 +9,25 @@ L=cv.imread("left.jpeg",0)
 R=cv.imread("right.jpeg",0)
 
 stereo = cv.StereoBM_create(numDisparities=128, blockSize=13)
-disparity = stereo.compute(L,R)
-plt.imshow(disparity,'gray')
+img = stereo.compute(L,R)
+plt.imshow(img,'gray')
 plt.show()
 
-hog = cv.HOGDescriptor()
-h = hog.compute(disparity)
-print(h.shape)
+winSize = (64,64)
+blockSize = (16,16)
+blockStride = (8,8)
+cellSize = (8,8)
+nbins = 9
+derivAperture = 1
+winSigma = 4.
+histogramNormType = 0
+L2HysThreshold = 2.0000000000000001e-01
+gammaCorrection = 0
+nlevels = 64
+hog = cv.HOGDescriptor(winSize,blockSize,blockStride,cellSize,nbins,derivAperture,winSigma,
+                        histogramNormType,L2HysThreshold,gammaCorrection,nlevels)
+#compute(img[, winStride[, padding[, locations]]]) -> descriptors
+winStride = (8,8)
+padding = (8,8)
+locations = ((10,20),)
+hist = hog.compute(img,winStride,padding,locations)
