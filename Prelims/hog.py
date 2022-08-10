dc75dc8be6cf6204e889e8b7aed98d69bccbf883
Rishi Nandha Vanchi <49914358+RishiNandha@users.noreply.github.com>
2022-08-11 01:15:26 +0530

    ersestest

--- Prelims/hog.py
+++ Prelims/hog.py
@@ -4,11 +4,15 @@ from skimage.feature import hog
 from skimage import exposure
 import matplotlib.pyplot as plt
 import cv2 as cv
+import numpy as np
 
-address_L = 'left.jpeg'
-address_R = 'right.jpeg'
+address_L = 'tsukuba_l.jpeg'
+address_R = 'tsukuba_r.jpeg'
 
 imgR = imread(address_R)
+
+if imgR.ndim==2:
+	imgR=imgR.reshape((imgR.shape[0],imgR.shape[1],1))
 
 fd, hog_image = hog(imgR, orientations=9, pixels_per_cell=(8, 8), 
                     cells_per_block=(2, 2), visualize=True, multichannel=True)
@@ -25,9 +29,17 @@ ax2.set_title('Histogram of Oriented Gradients')
 
 imgR_g = cv.imread(address_R, 0)
 imgL_g = cv.imread(address_L, 0)
-stereo = cv.StereoBM_create(numDisparities=128, blockSize=13)
+stereo = cv.StereoBM_create(numDisparities=16, blockSize=13)
 img = stereo.compute(imgL_g,imgR_g)
+#	img = exposure.rescale_intensity(img, in_range=(0, 10)) 
 ax3.imshow(img, cmap=plt.cm.gray)
 ax3.set_title('Depth Map')
+if img.ndim==2:
+	img=img.reshape((img.shape[0],imgR.shape[1],1))
+fd, hog2 = hog(img, orientations=9, pixels_per_cell=(8, 8), 
+                    cells_per_block=(2, 2), visualize=True, multichannel=True)
+hog2 = exposure.rescale_intensity(hog2, in_range=(0,10)) 
+ax4.imshow(hog2, cmap=plt.cm.gray)
+ax4.set_title('HOG over Depth Map')
 
 plt.show()
