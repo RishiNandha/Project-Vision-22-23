e7590989c7f815534df75dc745cb2313495ef1ed
Rishi Nandha Vanchi <49914358+RishiNandha@users.noreply.github.com>
2022-08-21 16:34:27 +0530

    Update mech.py

--- Prelims/mech.py
+++ Prelims/mech.py
@@ -1,22 +1,25 @@
 def generate(n):
 	ret=list()
-	for i in range(2**(n-4)):
+	for i in range(2**n):
 		str1=str(bin(i))
 		str1=str1[2:]
-		while (len(str1)<(n-4)):
-			str1 = '0'+str1
-		str1 = '0'+str1[0:2] + '0'+str1[2:4] + '0'+str1[4:6]+'1'+str1[6:]
+		while len(str1)<n:
+			str1='0'+str1
 		ret = ret + [str1,]
 	return ret
 
-def test(l):
+def test(l,n):
 	ret=list()
 	for i in range(8):
-		k = l[i:i+7:3]
+		k = l[i:i+2*n+1:n]
 		ret=ret+[k,]
 	return ret
 works = list()
-for i in generate(14):
-	if len(test(i))==len(set(test(i))):
+k=2
+for i in generate(8+2*k):
+	if len(test(i,k))==len(set(test(i,k))):
 		works = works + [i,]
-		print(test(i))
+		print(test(i,k))
+
+
+print(works)
