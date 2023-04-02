3d88dc10d1603c31ac5718292ad078bdc7438145
Rishi Nandha Vanchi <49914358+RishiNandha@users.noreply.github.com>
2023-04-02 11:29:17 +0530

    Update screen.py

--- Prelims/screen.py
+++ Prelims/screen.py
@@ -1,19 +1,24 @@
 import cv2 as cv
 import keyboard
 
+
+cam0 = cv.VideoCapture(0, cv.CAP_DSHOW)
+cam1 = cv.VideoCapture(1, cv.CAP_DSHOW)
+
 while(True):
-	p = int(input("Enter port number to preview: "))
-	cam = cv.VideoCapture(p)
-	while(True):
-		# Capture frame-by-frame
-		_, frame = cam.read()
-		try:
-			cv.imshow(f'preview_{p}',frame)
-		except:
-			print(f"Port {p} didn't work")
-			break
-		cv.waitKey(1)
-		# Waits for a user input to quit the application
-		if keyboard.is_pressed('q'):
-			cv.destroyAllWindows()
-			break
+	# Capture frame-by-frame
+	_, frame0 = cam0.read()
+	_, frame1 = cam1.read()
+	
+	try:
+		cv.imshow(f'preview_0',frame0)
+		cv.imshow(f'preview_1',frame1)
+	except:
+		print(f"Some port didn't work")
+		break
+	
+	cv.waitKey(1)
+	# Waits for a user input to quit the application
+	if keyboard.is_pressed('q'):
+		cv.destroyAllWindows()
+		break
